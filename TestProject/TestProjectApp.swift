046f2fd32536610455063a8af0dfece0f8d4b766
Maria Reyna <maria.reyna2244@gmail.com>
2026-09-11 09:18:03 -0400

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Maria Reyna on 9/11/26.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestProjectTests/TestProjectTests.swift
@@ -0,0 +1,19 @@
+//
+//  TestProjectTests.swift
+//  TestProjectTests
+//
+//  Created by Maria Reyna on 9/11/26.
+//
+
+import Testing
+@testable import TestProject
+
+struct TestProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
